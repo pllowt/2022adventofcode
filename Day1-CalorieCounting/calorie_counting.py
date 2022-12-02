768489d69bcec25cbf7f6bceb55d3c1c04e6f97e
Paul Lowther <paul.lowther01@hotmail.com>
2022-12-02 10:00:44 +0000

    add func calculate highest three vals

--- Day1-CalorieCounting/calorie_counting.py
+++ Day1-CalorieCounting/calorie_counting.py
@@ -7,7 +7,7 @@ def open_file():
         return list_of_inputs
 
 def add_inputs(list_inputs: list):
-    highest_value = 0
+    highest_value = 0 
     running_val = 0
     for val in list_inputs:
         if val:
@@ -17,6 +17,12 @@ def add_inputs(list_inputs: list):
         if running_val > highest_value:
             highest_value = running_val
     return highest_value
+
+def calculate_higher_value_in_list(value: int, vals_list: list):
+    for index, val in enumerate(vals_list):
+        if value > val:
+            vals_list[index] = value
+    return vals_list
 
 if __name__ == "__main__":
     list_inputs = open_file()
